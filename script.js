dc13dce2c880f0a666369e79e017538fcd4ffd85
jxjxj8101-ops <jxjxj8101@gmail.com>
2026-01-02 14:47:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 fetch("https://projectproero.vercel.app/balance", {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
+    headers: {
+        "Content-Type": "application/json"
+    },
     body: JSON.stringify({
         initData: Telegram.WebApp.initData
     })
 })
 .then(r => r.json())
-.then(d => {
-    document.getElementById("balance").innerText = d.balance;
+.then(data => {
+    document.getElementById("balance").innerText = data.balance;
+})
+.catch(err => {
+    console.error("Balance error:", err);
 });
